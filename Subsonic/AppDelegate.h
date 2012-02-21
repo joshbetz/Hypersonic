e1f8ac466486692f39c6f8da6e4f26d985d63a74
Josh Betz <betzster@gmail.com>
2012-02-21 09:51:45 -0600

    Initial Xcode project

---
+++ Subsonic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Subsonic
+//
+//  Created by Josh Betz on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Subsonic/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Subsonic
+//
+//  Created by Josh Betz on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
