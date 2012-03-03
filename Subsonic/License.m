09d0abb4b5ad8b2e9a3356ac8098a0ed68cfb98b
Josh Betz <betzster@gmail.com>
2012-03-03 13:45:42 -0600

    Moved files inside Subsonic folder

---
+++ Subsonic/License.m
@@ -0,0 +1,15 @@
+//
+//  License.m
+//  Subsonic
+//
+//  Created by Erin Rasmussen on 3/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "License.h"
+
+@implementation License
+
+@synthesize key;
+
+@end
