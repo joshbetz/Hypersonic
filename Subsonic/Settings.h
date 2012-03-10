0a2981cb74a3aa559ab872caa996a34fb9db9ef2
Josh Betz <betzster@gmail.com>
2012-03-09 22:27:36 -0600

    Add Settings view controller

---
+++ Subsonic/Settings.h
@@ -0,0 +1,13 @@
+//
+//  Settings.h
+//  Subsonic
+//
+//  Created by Josh Betz on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Settings : UITableViewController
+
+@end
