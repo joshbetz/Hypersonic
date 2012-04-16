b133a9b24f5e00e774691435df929a73d5103829
T. M <vegaslick@gmail.com>
2012-04-16 15:00:49 -0500

    search

--- Subsonic/ArtistTableViewController.h
+++ Subsonic/ArtistTableViewController.h
@@ -11,12 +11,20 @@
 
 @interface ArtistTableViewController : UITableViewController <EGORefreshTableHeaderDelegate, UITableViewDelegate, UITableViewDataSource>
 {
-	EGORefreshTableHeaderView *_refreshHeaderView;
+    IBOutlet UISearchDisplayController *artistSearchDisplayController;
+    IBOutlet UISearchBar *artistSearchBar;
+    NSMutableArray *itemsFromCurrentSearch;
+	
+    EGORefreshTableHeaderView *_refreshHeaderView;
 	
 	//  Reloading var should really be your tableviews datasource
 	//  Putting it here for demo purposes 
 	BOOL _reloading;
 }
+@property (strong, nonatomic) IBOutlet UISearchDisplayController *artistSearchDisplayController;
+@property (strong, nonatomic) IBOutlet UISearchBar *artistSearchBar;
+@property (strong, nonatomic) NSMutableArray *itemsFromCurrentSearch;
+
 
 - (void)reloadTableViewDataSource;
 - (void)doneLoadingTableViewData;
--- Subsonic/ArtistTableViewController.m
+++ Subsonic/ArtistTableViewController.m
@@ -15,6 +15,9 @@
 #import "NowPlaying.h"
 
 @implementation ArtistTableViewController
+@synthesize artistSearchDisplayController = _artistSearchDisplayController;
+@synthesize artistSearchBar = _artistSearchBar;
+@synthesize itemsFromCurrentSearch;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -49,12 +52,19 @@
 	
 	//  update the last update date
 	[_refreshHeaderView refreshLastUpdatedDate];
-    
+
+    itemsFromCurrentSearch = [NSMutableArray array];
 }
 
 - (void)viewDidUnload
 {
+    //Initializing SearchBar components
+    //[self setArtistSearchBar:nil];
+    //artistSearchBar = nil;
+    //artistSearchDisplayController = nil;
+    //[self setArtistSearchDisplayController:nil];
     [super viewDidUnload];
+    
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
@@ -107,9 +117,31 @@
 {
     if ([[segue identifier] isEqualToString:@"AlbumClick"]) {
         AlbumSongTableViewController *nextViewController = [segue destinationViewController];
+        
         NSString *directoryID = [[[artistList objectAtIndex:[self.tableView indexPathForSelectedRow].section]objectAtIndex:[self.tableView indexPathForSelectedRow].row ] artistID];
         nextViewController.userURL = [NSString stringWithFormat:@"%@&id=%@", [AppDelegate getEndpoint:@"getMusicDirectory"], directoryID];
+    } else {
+        AlbumSongTableViewController *nextViewController = [segue destinationViewController];
+        
+        NSString *directoryID = [[self.itemsFromCurrentSearch objectAtIndex:[self.tableView indexPathForSelectedRow].row ] artistID];
+        nextViewController.userURL = [NSString stringWithFormat:@"%@&id=%@", [AppDelegate getEndpoint:@"getMusicDirectory"], directoryID];
     }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    //Store *storeToDetail = nil;
+    if (tableView == self.searchDisplayController.searchResultsTableView)
+    {
+        [self performSegueWithIdentifier:@"FromSearch" sender:self];
+    }
+    else
+    {
+        [self performSegueWithIdentifier:@"AlbumClick" sender:self];
+    }
+    
+    // How to get segue destination controller and set object?!
+    // Before doing:
 }
 
 #pragma mark - Table view data source
@@ -117,13 +149,29 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return [artistList count];
+    if([tableView isEqual:self.artistSearchDisplayController.searchResultsTableView]){
+        //return [self.itemsFromCurrentSearch count];
+        return 1;
+    }else{
+        
+        return [artistList count];
+    }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return [[artistList objectAtIndex:section] count];
+    //This function will also be used for the SearchDisplay so following
+    //Conditional checks which rows are being returned and responds
+    //accordingly
+    if ([tableView 
+         isEqual:self.artistSearchDisplayController.searchResultsTableView]){
+        return [self.itemsFromCurrentSearch count];
+    
+    }else{
+        
+        return [[artistList objectAtIndex:section] count];
+    }
 }
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
@@ -254,7 +302,15 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
     
-    cell.textLabel.text = [NSString stringWithFormat:@"%@", [[[artistList objectAtIndex: indexPath.section]objectAtIndex:indexPath.row] artistName] ];
+    //Following conditional returns cell depending on which table is 
+    //being populated
+    if ([tableView isEqual:self.searchDisplayController.searchResultsTableView]){
+        cell.textLabel.text =[NSString stringWithFormat:@"%@", [[self.itemsFromCurrentSearch objectAtIndex:indexPath.row] artistName]];
+    
+    }else{
+        cell.textLabel.text = [NSString stringWithFormat:@"%@", [[[artistList objectAtIndex: indexPath.section]objectAtIndex:indexPath.row] artistName] ];
+        
+    }
     // Configure the cell...
     
     return cell;
@@ -319,5 +375,153 @@
 	return [NSDate date]; // should return date data source was last changed
 	
 }
+
+//Callback function made when the searchbar text changes
+//This can be implemented 2 ways
+//1. Search through the values we already have
+//2. Hit the server
+- (void)filterContentForSearchText:(NSString*)searchText 
+                             scope:(NSString*)scope
+{
+    //NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"(artistName) contains[cd] %@",searchText];
+    
+    if(![searchText isEqualToString:@""]){
+        UniChar alph = [[searchText lowercaseString] characterAtIndex:0];
+        NSInteger arrIndex = -1;
+        
+        if(alph == 'a'){
+            arrIndex = 0;
+        
+        }else if (alph == 'b') {
+            arrIndex = 1;
+        
+        }else if (alph == 'c') {
+            arrIndex = 2;
+        
+        }else if (alph == 'd') {
+            arrIndex = 3;
+            
+        }else if (alph == 'e') {
+            arrIndex = 4;
+            
+        }else if (alph == 'f') {
+            arrIndex = 5;
+            
+        }else if (alph == 'g') {
+            arrIndex = 6;
+            
+        }else if (alph == 'h') {
+            arrIndex = 7;
+            
+        }else if (alph == 'i') {
+            arrIndex = 8;
+            
+        }else if (alph == 'j') {
+            arrIndex = 9;
+            
+        }else if (alph == 'k') {
+            arrIndex = 10;
+            
+        }else if (alph == 'l') {
+            arrIndex = 11;
+            
+        }else if (alph == 'm') {
+            arrIndex = 12;
+            
+        }else if (alph == 'n') {
+            arrIndex = 13;
+            
+        }else if (alph == 'o') {
+            arrIndex = 14;
+            
+        }else if (alph == 'p') {
+            arrIndex = 15;
+            
+        }else if (alph == 'q') {
+            arrIndex = 16;
+            
+        }else if (alph == 'r') {
+            arrIndex = 17;
+            
+        }else if (alph == 's') {
+            arrIndex = 18;
+            
+        }else if (alph == 't') {
+            arrIndex = 19;
+            
+        }else if (alph == 'u') {
+            arrIndex = 20;
+            
+        }else if (alph == 'v') {
+            arrIndex = 21;
+            
+        }else if (alph == 'w') {
+            arrIndex = 22;
+            
+        }else if((alph == 'x') || (alph == 'y') || (alph == 'z')){
+            arrIndex = 23;
+        } else {
+            arrIndex = 24;
+        }
+
+
+
+
+
+
+        
+        NSInteger i = 0;
+        NSMutableArray *temp;
+        NSString *temp1;
+        
+        self.itemsFromCurrentSearch = [NSMutableArray array];
+        
+        if(arrIndex != -1){
+            temp = [[NSMutableArray alloc] initWithArray:[artistList objectAtIndex:arrIndex]];
+
+            for(i = 0; i < [[artistList objectAtIndex:arrIndex] count]; i++){
+                temp1 = [[[temp objectAtIndex:i] artistName] lowercaseString];
+                //searchText = [searchText lowercaseString];
+                NSRange range = [temp1 rangeOfString:searchText options:NSCaseInsensitiveSearch];
+                
+                if (range.location != NSNotFound){
+                    [self.itemsFromCurrentSearch addObject:[temp objectAtIndex:i]];
+                }
+            }
+        }
+    }
+    /*if(arrIndex != -1){
+        self.itemsFromCurrentSearch = [[NSMutableArray alloc] initWithArray:artistList copyItems:YES];
+        NSLog(@"%@", itemsFromCurrentSearch);
+        itemsFromCurrentSearch = [self.itemsFromCurrentSearch objectAtIndex:arrIndex];
+        NSLog(@"%@", self.itemsFromCurrentSearch);
+        [self.itemsFromCurrentSearch filterUsingPredicate:resultPredicate];
+        //NSLog(@"%@", self.itemsFromCurrentSearch);
+        //NSLog(@"%@", artistList);
+    }*/
+}
+
+#pragma mark - UISearchDisplayController delegate methods
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller 
+shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString 
+                               scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
+                                      objectAtIndex:[self.searchDisplayController.searchBar
+                                                     selectedScopeButtonIndex]]];
+    
+    return YES;
+}
+
+/*- (BOOL)searchDisplayController:(UISearchDisplayController *)controller 
+shouldReloadTableForSearchScope:(NSInteger)searchOption
+{
+    [self filterContentForSearchText:[self.searchDisplayController.searchBar text] 
+                               scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
+                                      objectAtIndex:searchOption]];
+    
+    return YES;
+}*/
+
 
 @end
